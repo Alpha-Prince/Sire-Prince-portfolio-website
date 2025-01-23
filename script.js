8b4be00e00ec8621d7a953821ee8fa63b9f1ebae
Sire Prince <153405197+Alpha-Prince@users.noreply.github.com>
2025-01-22 13:30:06 -1200

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,44 @@
-let menuIcon = document.querySelector(".menu-icon")
-let sidebar = document.querySelector(".sidebar")
-let container = document.querySelector(".container")
+let humburgur = document.querySelector('.humburger');
+let navMenu = document.querySelector('.nav-menu');
+humburgur.addEventListener('click', () => {
+    humburgur.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+)
 
+document.querySelectorAll('.nav_item').forEach(n => n.addEventListener('click', ()=>{
+    humburgur.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
 
-menuIcon.onclick = function(){
-    sidebar.classList.toggle("small-sidebar")
-    container.classList.toggle("large-container")
+document.querySelectorAll('.nav_item').forEach(n => n.addEventListener('click', ()=>{
+    humburgur.classList.remove("active");
+    navMenu.classList.remove("active");
+}))
+/* ------------------------control container(cn)------------------------ */
+let cnHumburgur = document.querySelector('.cn-humburger');
+let cnContainer = document.querySelector('.cn-container');
+cnHumburgur.addEventListener('click', () => {
+    cnHumburgur.classList.toggle("active");
+    cnContainer.classList.toggle("active");
+}
+)
+
+/* ------------------------dark mode------------------------ */
+let cnmode = document.querySelector('.cn-mode');
+ cnmode.onclick = function(){
+    cnmode.classList.toggle("dark-mode-on");
+    document.body.classList.toggle("dark-theme");
+    cnContainer.classList.toggle("active");
+    cnHumburgur.classList.toggle("active");
+}
+
+let mode = document.querySelector('.mode');
+ mode.onclick = function(){
+    mode.classList.toggle("dark-mode-on");
+    document.body.classList.toggle("dark-theme");
+    navMenu.classList.remove("active");
+    humburgur.classList.remove("active");
 }
 
 
